5ded3ef0cba4d2f5373feec3e4c0fb91ecec096a
Michael Griebling <michael.griebling@solinst.com>
2018-02-15 14:22:12 -0500

    Initial Commit

---
+++ Resistor Box/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Resistor Box
+//
+//  Created by Michael Griebling on 2018-02-15.
+//  Copyright © 2018 Solinst Canada. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Resistor Box/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Resistor Box
+//
+//  Created by Michael Griebling on 2018-02-15.
+//  Copyright © 2018 Solinst Canada. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
