cc4ef4d2da0a13823a72741e219ff4f93b4b246c
Michael Griebling <michael.griebling@solinst.com>
2018-04-17 08:36:18 -0400

    Added user settings to Mac version.

---
+++ Resistor Box Mac/UserSettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UserSettingsViewController.swift
+//  Resistor Box Mac
+//
+//  Created by Michael Griebling on 2018-04-17.
+//  Copyright © 2018 Solinst Canada. All rights reserved.
+//
+
+import Cocoa
+
+class UserSettingsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
